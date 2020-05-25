8416c279e0a5fa63fb3ba17ee81c51d63f39273c
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2020-05-25 13:11:42 +0100

    Add about page first attempt

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { AboutPageTemplate } from '../../templates/about-page'
+import React from 'react';
+import { AboutPageTemplate } from '../../templates/about-page';
 
-const AboutPagePreview = ({ }) => (
-  <AboutPageTemplate
-   
-  />
-)
+const AboutPagePreview = ({ entry }) => {
+  const data = entry.getIn(['data']).toJS();
 
+  if (data) {
+    return <AboutPageTemplate content={data.content} />;
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
-
-export default AboutPagePreview
+export default AboutPagePreview;
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from 'react';
+import { graphql } from 'gatsby';
 import ReactMarkdown from 'react-markdown';
 import Layout from '../components/Layout/Layout';
-import globalStyles from '../components/global.module.css';
+import '../components/global.module.css';
+import aboutStyles from './about.module.css';
 
-export const AboutPageTemplate = ({ }) => {
+export const AboutPageTemplate = ({ content }) => {
+  return (
+    <div className={aboutStyles.wrapper}>
+      <ReactMarkdown source={content} />
+    </div>
+  );
+};
 
+const AboutPage = ({ data = {} }) => {
+  const { about = { nodes: [] }, home = { nodes: [] } } = data;
+  const { frontmatter: homeDetail = {} } = home.nodes[0];
+  const { headerLogo, socialLinks } = homeDetail;
+
+  const { frontmatter = {} } = about.nodes[0];
 
   return (
-    <>
-    </>
-  )
-}
-
-
-
-const AboutPage = ({  }) => {
-
-
-  return (
-    <Layout>
-      <AboutPageTemplate
-       
-      />
+    <Layout headerLogoUrl={headerLogo.publicURL} socialLinks={socialLinks}>
+      <AboutPageTemplate content={frontmatter.content} />
     </Layout>
-  )
-}
+  );
+};
 
-
-
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     about: allMarkdownRemark(filter: { id: { eq: $id } }) {
       nodes {
         frontmatter {
-          blurb
+          content
         }
       }
     }
